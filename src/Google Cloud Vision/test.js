5622a249f3ee6a702cac0d2d9a8124cd62ac6418
Nick Liu <nickliu02@gmail.com>
2020-07-18 15:00:51 -0400

    lmao

---
+++ src/Google Cloud Vision/test.js
@@ -0,0 +1,17 @@
+async function quickstart() {
+    // Imports the Google Cloud client library
+    const vision = require('@google-cloud/vision');
+  
+    // Creates a client
+    const client = new vision.ImageAnnotatorClient({
+        keyFilename: "APIKey.json"
+    });
+  
+    // Performs label detection on the image file
+    const [result] = await client.labelDetection('test.png');
+    const labels = result.labelAnnotations;
+    console.log('Labels:');
+    labels.forEach(label => console.log(label.description));
+}
+
+quickstart();
