5a43df70f2c9d4a84369e6de75aefddd50b68913
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-07-18 20:23:35 -0400

    finally made goodreads requerst

--- src/schema.js
+++ src/schema.js
@@ -1,16 +1,6 @@
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLList } = require('graphql');
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLList, GraphQLID } = require('graphql');
 // const { resolveFieldValueOrError } = require('graphql/execution/execute'); 
-const { getBook } = require('./services/api.service.js');
-
-const UserType = new GraphQLObjectType({
-    name: 'User',
-    fields: () => ({
-        id: { type: GraphQLInt },
-        name: { type: GraphQLString },
-        username: { type: GraphQLString },
-        email: { type: GraphQLString },
-    })
-});
+// const { getBook } = require('./services/api.service.js');
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -26,11 +16,9 @@ const RootQuery = new GraphQLObjectType({
             type: BookType,
             args: { title: { type: GraphQLString }},
             resolve(root, args) {
-                return getBook(args.title).then(response => {
-                    const result = response.GoodreadsResponse.search[0].results[0].work[0].best_book[0];
-                    console.log(result)
-                    return result
-                });
+                // return getBook(args.title).then(response => {
+                //     return response.GoodreadsResponse.search[0].results[0].work[0].best_book[0];
+                // });
             }
         }
     }
