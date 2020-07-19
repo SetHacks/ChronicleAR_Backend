4c2ab2e5560321c341829bb495c738ca492803f8
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-07-18 21:27:53 -0400

    author schema

--- src/schema.js
+++ src/schema.js
@@ -8,6 +8,7 @@ const getXML = key => obj => obj[key][0];
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
+        id: { type: GraphQLID, resolve: getXML('id')  }, 
         title: { type: GraphQLString, resolve: getXML('title')  }, 
         publication_year: { type: GraphQLString, resolve: getXML('publication_year')  }, 
         publisher: { type: GraphQLString, resolve: getXML('publisher')  }, 
@@ -15,9 +16,24 @@ const BookType = new GraphQLObjectType({
         is_ebook: { type: GraphQLString, resolve: getXML('is_ebook')  },
         description: { type: GraphQLString, resolve: getXML('description')  },
 
+        authors: {
+            type: new GraphQLList(AuthorType),
+            resolve(parent,args) {
+                return parent.authors.map(author => author.author[0])
+            }
+        },
+
         average_rating: { type: GraphQLString, resolve: getXML('average_rating')  },
         num_pages: { type: GraphQLString, resolve: getXML('num_pages')  }, 
 
+    })
+});
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: { type: GraphQLID, resolve: getXML('id') }, 
+        name: { type: GraphQLString, resolve: getXML('name') }, 
     })
 });
 
